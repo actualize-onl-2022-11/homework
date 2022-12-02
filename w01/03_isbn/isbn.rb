4e2ac53798b4314618664d8566db043b63751c54
Alex Lam <alam.seraph@gmail.com>
2022-12-01 17:50:31 -0800

    decomposed the problem into parts

--- w01/03_isbn/isbn.rb
+++ w01/03_isbn/isbn.rb
@@ -1,7 +1,79 @@
 def isbn_verify?(isbn_string)
   # Your job is to write the code for this method!
-  isbn_string.each_char{|ch| p ch}
-  isbn_string.size
-  
+  isbn_string = isbn_string.delete "-"
+
+  if isbn_string.size <= 10
+
+  end 
+ 
+   
 end
 isbn_verify?("3-598-21508-8")
+
+
+
+
+# Ensures character in string are numbers
+# isbn_string.each_char{|ch| p ch
+#   if (1..9).include?(ch)
+#     return "true"
+#   else 
+#     return "Invalid"
+#   end  }
+# else 
+#   p "Invalid."
+# end 
+
+# TEST 
+# def isbn_verify?(isbn_string)
+#   array = []
+# isbn_string.each_char{ |ch| p ch
+#   if (1..9).include?(ch)
+#     array << ch
+#   end  }
+#   p array
+# end 
+
+# ADDS STRING TO AN ARRAY AND CALCULATE SUM
+# array = []
+# x = isbn_string.scan /\w/
+# array << x
+# p array 
+#   sum = 0 
+#   y = 0 
+#   while y < array[0].length
+#     sum = sum + array[0][y].to_i 
+#     y += 1
+#   end 
+#   p sum 
+
+
+# Checks ISBN for VALIDITY
+# def isbn_verify?(isbn_string)
+#   array = []
+#   x = isbn_string.scan /\w/
+#   array << x
+#   p array 
+  
+#     collective = []
+#     t = 0 
+#     n = 10
+#     while t < array[0].length
+#       number = array[0][t].to_i * n 
+#       t += 1 
+#       n = n - 1 
+#       collective << number
+#       puts number
+#     end 
+    
+#     sum = 0 
+#     y = 0 
+#     while y < collective.length
+#       sum = sum + collective[y].to_i 
+#       y += 1
+#     end 
+#     if sum  % 11 == 0 
+#       p "This is a valid ISBN number"
+#     end 
+#   end 
+  
