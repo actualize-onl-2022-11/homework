34628e47c67681d569640f81249ebb58104eef86
Makayla <makaylabusher@gmail.com>
2022-12-02 14:29:31 -0600

    create code to calculate grade

--- w01/01_grades/grades.rb
+++ w01/01_grades/grades.rb
@@ -1,6 +1,29 @@
 def final_grade(array)
+  sum = 0 
+  array.each do |grade|
+    sum += grade
+end
 
-  # This method currently has no code.
-  # Your job is to write the code to get the method to work!
+if array == []
+  puts "I"
+else
+  average = sum / array.length
+end
 
+if average.to_i > 100
+  puts "Error, cannot exceed 100"
+elsif average.to_i >= 90
+  grade = "A"
+elsif average.to_i >= 80
+  grade = "B"
+elsif average.to_i >= 70
+  grade = "C"
+elsif average.to_i >= 60
+  grade = "D"
+elsif average.to_i >= 0
+  grade = "F"
+else
+  puts "Error, grades cannot go below 0"
+end
+ p grade 
 end
