99c5748c12831e89945f75c60fec6f9f129431c6
“johnryanmal” <johnryanmal@gmail.com>
2022-12-04 16:39:00 -0800

    make and test bob_responds

--- w01/02_bob/bob.rb
+++ w01/02_bob/bob.rb
@@ -1,3 +1,15 @@
-def bob_responds(sentence)
-  # Your job is to write the code for this method!
+def bob_responds(string)
+  sentence = string.strip
+  last = sentence[-1]
+  alpha = (sentence.downcase != sentence.upcase)
+  caps = (sentence == sentence.upcase)
+
+  return 'Fine, be that way.' if sentence.empty?
+  return 'Calm down, I know what I’m doing!' if alpha && caps && last == '?'
+  return 'Why are you yelling at me?' if alpha && caps
+  return 'Sounds good.' if last == '.'
+  return 'Whoa, chill out!' if last == '!'
+  return 'Sure.' if last == '?'
+
+  return 'Whatever.'
 end
