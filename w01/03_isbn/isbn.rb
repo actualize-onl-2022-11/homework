7189483149edfcd3ba365575644a23026b9eea29
“johnryanmal” <johnryanmal@gmail.com>
2022-12-04 17:29:15 -0800

    write isbn verification

--- w01/03_isbn/isbn.rb
+++ w01/03_isbn/isbn.rb
@@ -1,3 +1,25 @@
 def isbn_verify?(isbn_string)
-  # Your job is to write the code for this method!
+  isbn = isbn_string.gsub('-', '').upcase
+  return false unless isbn.match? /^[0-9]{9}[0-9X]$/
+
+  map = {
+    '0' => 0,
+    '1' => 1,
+    '2' => 2,
+    '3' => 3,
+    '4' => 4,
+    '5' => 5,
+    '6' => 6,
+    '7' => 7,
+    '8' => 8,
+    '9' => 9,
+    'X' => 10
+  }
+
+  sum = 0
+  10.times do |index|
+    sum += (10-index) * map[isbn[index]]
+  end
+
+  return (sum % 11 == 0)
 end
