72bac8acc76bcbcf1b6645975a59b9e3277bcd90
Alex Lam <alam.seraph@gmail.com>
2022-12-07 16:31:04 -0800

    finished exe1

--- w02/09_hash_as_index/hash_as_index.rb
+++ w02/09_hash_as_index/hash_as_index.rb
@@ -1,6 +1,20 @@
 def intersection(array_1, array_2)
   # Exercise 1: You need to complete this method.
+  arr = []
+  index1 = 0 
+  while index1 < array_1.length
+    index2 = 0 
+    while index2 < array_2.length
+      if array_2[index2] == array_1[index1]
+        arr << array_1[index1]
+      end
+      index2 += 1
+    end 
+    index1 += 1
+  end 
+  p arr
 end
+
 
 def find_first_duplicate(array)
   # Exercise 2: You need to complete this method!
