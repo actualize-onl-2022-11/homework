0246f0bbd55ed93c29b78c203929b0af3ed9035c
Makayla <makaylabusher@gmail.com>
2022-12-12 22:47:04 -0600

    create method + run tests

--- w01/02_bob/bob.rb
+++ w01/02_bob/bob.rb
@@ -1,4 +1,5 @@
 def bob_responds(sentence)
+
   if sentence.include? (".") || (". ")
     p "Sounds good."
   elsif sentence.include? ("!") || ("! ")
@@ -11,12 +12,18 @@ def bob_responds(sentence)
     end
   elsif sentence.include? ("  ")
     p "Fine, be that way."
-  elsif sentence.include? (sentence.upcase)
-    p "Why are you yelling at me?"
-  elsif 
-    p "Whatever."
-  else 
+  elsif sentence.include? (sentence.upcase) 
+    if (sentence.include? (sentence.upcase)) && (sentence.count("0-9") > 0 )
+      if sentence.count("a-zA-Z") > 0
+        p "Why are you yelling at me?"
+      else
+        p "Whatever."
+      end
+    else
+      p "Why are you yelling at me?"
+    end
+  else
     p "Whatever."
   end
+
 end
-bob_responds("CAN YOU HAVE THIS TO ME BY FRIDAY?")
